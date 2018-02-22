7cae6ce115aacf94d95456fbe5e59aa4c710f639
Daniel Shimon <danielzshimon@gmail.com>
2018-02-22 21:44:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts(arrayOfFacts){
   var finalArray = [];
   var i = 0;
   for(i = 0; i< arrayOfFacts.length; i++)   { 
-    finalArray.push(arrayOfFacts[i]) + "!!!";
+    finalArray.push(arrayOfFacts[i] + "!!!");
   }
   return finalArray;
 }
